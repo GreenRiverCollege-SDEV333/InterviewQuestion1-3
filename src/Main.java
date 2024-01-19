8fb525841ce26dfeddfcf6ae44908f67686aa2c7
tobygoetz <goetz.tobiah@student.greenriver.edu>
2024-01-18 18:56:28 -0800

    Finished solution

--- src/Main.java
+++ src/Main.java
@@ -35,9 +35,17 @@
 
 import java.util.Arrays;
 
+/**
+ * Driver class that defines the length of a Buffer and calls
+ * a main method
+ */
 public class Main {
     public static final int BUFFER_CAPACITY = 32768;
 
+    /**
+     * program
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -45,7 +53,7 @@ public class Main {
 
         // set up empty buffer, size of 0
         char[] buffer = new char[BUFFER_CAPACITY];
-        int size = 0;
+        int size;
 
         // initialize the buffer and size variables with some data
         String temp = "Dr Martin Luther King";
@@ -59,14 +67,55 @@ public class Main {
         System.out.println("size: " + size);
 
         // call your method here
+        size = urlify(buffer, size);
+
 
         // check the "after" buffer contents via println
+        System.out.println(Arrays.toString(buffer));
+
         // check to see if the new buffer's size is correct
+        System.out.println("size: " + size);
 
+        int counter = 0;
+        int index = 0;
+        while (buffer[index] != '\u0000') {
+            counter++;
+            index++;
+        }
 
+        if (counter == size) {
+            System.out.println("Size is accurate");
+        } else {
+            System.out.println("Size is not accurate");
+        }
     }
 
     // write your method here
 
+    /**
+     * Method that adds a '%', a '2' and a '0' character for every space
+     * character found in a char[] to %20 and shifts the remaining characters
+     * over to include room for these new characters
+     * @param buffer char[]
+     * @param size Amount of non-empty indices in buffer char[]
+     * @return updated size of buffer char[]
+     */
+    public static int urlify(char[] buffer, int size) {
 
+        for (int i = 0; i < size; i++) {
+            if (buffer[i] == ' ') {
+
+                size += 2;
+
+                for (int j = size; j > i; j--) {
+                    buffer[j] = buffer[j - 2];
+                }
+
+                buffer[i] = '%';
+                buffer[i + 1] = '2';
+                buffer[i + 2] = '0';
+                i = i + 2;
+            }
+        } return size;
+    }
 }
